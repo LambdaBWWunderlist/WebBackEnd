3b1295ac155c2bc9fc5bb77763361312c5b19551
unknown <dethgrenade@gmail.com>
2020-06-20 11:49:31 -0700

    updated migration timestamps

--- data/migrations/20200619180010_init_db_tables.js
+++ data/migrations/20200619180010_init_db_tables.js
@@ -8,7 +8,8 @@ exports.up = function (knex) {
             tbl.string('username', 128).notNullable().unique().index()
             tbl.string('password', 256).notNullable()
             tbl.string('email', 128).notNullable().unique()
-            tbl.timestamp('created_at').defaultsTo(knex.fn.now())
+            tbl.timestamp('created_at', { useTz: false }).defaultsTo(knex.fn.now())
+            tbl.timestamp('updated_at', { useTz: false }).defaultsTo(knex.fn.now())
         })
         .createTable('items', tbl => {
             tbl.increments()
@@ -16,7 +17,8 @@ exports.up = function (knex) {
             tbl.string('name', 128).notNullable().index()
             tbl.boolean('completed', 256).notNullable().defaultsTo(false)
             tbl.string('recurring').defaultsTo(null)
-            tbl.timestamps(true, true)
+            tbl.timestamp('created_at', { useTz: false }).defaultsTo(knex.fn.now())
+            tbl.timestamp('updated_at', { useTz: false }).defaultsTo(knex.fn.now())
 
             //Foreign Key user_id => users.id
             tbl.integer('user_id')
