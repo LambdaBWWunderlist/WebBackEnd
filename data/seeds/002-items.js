724a0d06c2b8f726e6a25e7d8c4900e0651d3b9b
unknown <dethgrenade@gmail.com>
2020-06-22 12:19:34 -0700

    added knex-cleaner to project and setup sleaner in seeds

--- data/seeds/002-items.js
+++ data/seeds/002-items.js
@@ -1,39 +1,34 @@
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex
-    .raw('TRUNCATE users RESTART IDENTITY CASCADE')
-    .then(function () {
-      // Inserts seed entries
-      return knex('items').insert([
-        {
-          name: 'Assemble the avengers',
-          user_id: 1
-        },
-        {
-          name: 'Become Heavy weight Champ',
-          user_id: 2
-        },
-        {
-          name: 'Solve time travel',
-          user_id: 1
-        },
-        {
-          name: 'Win Royale Rumble',
-          user_id: 2
-        },
-        {
-          name: 'Get the Infinity Stones',
-          user_id: 1
-        },
-        {
-          name: '?????',
-          user_id: 1
-        },
-        {
-          name: 'Profit',
-          user_id: 1
-        }
-      ]);
-    });
+  return knex('items').insert([
+    {
+      name: 'Assemble the avengers',
+      user_id: 1
+    },
+    {
+      name: 'Become Heavy weight Champ',
+      user_id: 2
+    },
+    {
+      name: 'Solve time travel',
+      user_id: 1
+    },
+    {
+      name: 'Win Royale Rumble',
+      user_id: 2
+    },
+    {
+      name: 'Get the Infinity Stones',
+      user_id: 1
+    },
+    {
+      name: '?????',
+      user_id: 1
+    },
+    {
+      name: 'Profit',
+      user_id: 1
+    }
+  ]);
 };
